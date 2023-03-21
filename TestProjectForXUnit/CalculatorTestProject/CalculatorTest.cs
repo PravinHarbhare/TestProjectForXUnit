90bb7a525cd40e71a5f62e2723ace839d494ae65
PravinHarbhare <91001803+PravinHarbhare@users.noreply.github.com>
2023-03-21 16:32:27 +0530

    added

---
+++ TestProjectForXUnit/CalculatorTestProject/CalculatorTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using TestProjectForXUnit;
+using Xunit;
+
+namespace CalculatorTestProject
+{
+    public class CalculatorTest
+    {
+        Calculator cal = new Calculator();
+
+        [Fact]
+        public void Add_CheckForValid()
+        {
+            //Arrange
+
+            int a = 2;
+            int b = 3;
+
+            //Act
+            int result  = cal.Add(2, 3);
+
+            //Assert
+
+            Assert.Equal(5, result);
+
+
+        }
+
+
+        [Fact]
+        public void Add_CheckForInValid()
+        {
+            //Arrange
+
+            int a = 2;
+            int b = 3;
+
+            //Act
+            int result = cal.Add(2, 3);
+
+            //Assert
+
+            Assert.Equal(6, result);
+        }
+    }
+}
---
+++ TestProjectForXUnit/TestProjectForXUnit/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TestProjectForXUnit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator cal = new Calculator();
+            cal.Add(3, 5);
+        }
+    }
+}
